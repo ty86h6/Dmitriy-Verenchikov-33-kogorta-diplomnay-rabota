b6491e5a7469a0e40465a2065a2456ae154dea80
ty86h6 <dmitry.verenchikov+1@yandex.ru>
2025-08-21 17:18:21 +0300

    Create sender_stand_test.py

---
+++ sender_stand_test.py
@@ -0,0 +1,13 @@
+# Автотест
+def test_order_creation_and_retrieval():
+    response = create_order(data.order_body)
+
+    track_number = response.json()["track"]
+   
+
+    # Получение данных заказа по треку
+    order_response = get_order(track_number)
+
+    assert order_response.status_code == 200, f"Ошибка: {order_response.status_code}"
+    order_data = order_response.json()
+    
