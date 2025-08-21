945fc408223442d983ac2cafccde9685b42f7eed
ty86h6 <dmitry.verenchikov+1@yandex.ru>
2025-08-21 17:17:40 +0300

    Update sendor_stand_request.py

--- sendor_stand_request.py
+++ sendor_stand_request.py
@@ -13,16 +13,4 @@ def get_order(track_number):
     response = requests.get(get_order_url)
     return response
 
-# Автотест
-def test_order_creation_and_retrieval():
-    response = create_order(data.order_body)
-
-    track_number = response.json()["track"]
-   
-
-    # Получение данных заказа по треку
-    order_response = get_order(track_number)
-
-    assert order_response.status_code == 200, f"Ошибка: {order_response.status_code}"
-    order_data = order_response.json()
     
