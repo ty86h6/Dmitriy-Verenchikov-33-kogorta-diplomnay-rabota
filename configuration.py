a901a62d6578310ac142cba150227c75ef7ded05
ty86h6 <dmitry.verenchikov+1@yandex.ru>
2025-08-24 13:13:43 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,5 @@
 URL_SERVICE =  " https://080c58d0-f2c0-469c-92e1-d9587e869ce1.serverhub.praktikum-services.ru"
 CREATE_ORDERS = "/api/v1/orders"
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
+
 
