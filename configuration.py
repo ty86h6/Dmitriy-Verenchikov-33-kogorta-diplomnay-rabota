e6b7ff5c84a5b158179352e31195b069e162ac7a
ty86h6 <dmitry.verenchikov+1@yandex.ru>
2025-08-29 14:38:30 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,7 @@
 URL_SERVICE =  " https://99e2eb31-4f09-4019-abc4-2c36e873b49c.serverhub.praktikum-services.ru"
 CREATE_ORDERS = "/api/v1/orders"
-GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track?t=860837"
+
 
 
 
